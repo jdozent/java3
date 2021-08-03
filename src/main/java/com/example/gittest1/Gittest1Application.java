5cac2723533f797e13586a52f9e5017ce632b553
DESKTOP-13J4M6T\micablue <micha.schirmer@gmail.com>
2021-08-03 15:49:31 +0200

    init

---
+++ src/main/java/com/example/gittest1/Gittest1Application.java
@@ -0,0 +1,15 @@
+package com.example.gittest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittest1Application {
+
+    // Main
+    public static void main(String[] args) {
+        SpringApplication.run(Gittest1Application.class, args);
+
+    }
+
+}
